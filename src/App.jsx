82ab5b880145c8a566fe3398e7f3e5bdc40716c9
joana faria <paisdefaria.joana@gmail.com>
2024-02-26 18:53:52 +0000

    random dice generated by button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
 import { useState } from 'react'
 import Dice from './components/Dice'
+import {nanoid} from "nanoid"
 import './App.css'
 
 export default function App() {
 
+  const [dice, setDice] = useState(GenerateDice())
+
+  function GenerateDice() {
+
+    function RandNum(){
+      return (Math.ceil(Math.random() * 6))
+    }
+
+    const newArray = []
+    for (let i = 0; i < 10; i++){
+      newArray.push({
+        value:RandNum(),
+        isLocked:false,
+        id: nanoid()
+      })
+    }
+    return newArray
+  }
+
+
+    function handleClick(){
+      setDice(GenerateDice())
+    }
+
+    const diceElements = dice.map(element => <Dice key={element.id} value={element.value}/>)
+
   return (
     <main>
-    <div className="dice-grid">
-        <Dice value= {1} />
-        <Dice value= {2} />
-        <Dice value= {3} />
-        <Dice value= {4} />
-        <Dice value= {5} />
-        <Dice value= {6} />
-        <Dice value= {7} />
-        <Dice value= {8} />
-        <Dice value= {9} />
-        <Dice value= {10} />
-    </div>
+      <div className="dice-grid">
+          {diceElements}
+      </div>
 
-</main>
+      <button className="roll-button"
+      onClick={handleClick}>
+        Roll
+      </button>
+
+    </main>
 
   )
 }
